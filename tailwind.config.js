f76ed375dab324ab198ec1629649c37c6728b220
Andres De la Cruz <hugoandres2205@gmail.com>
2024-07-29 12:38:05 -0500

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "quicksand": ["Quicksand", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
